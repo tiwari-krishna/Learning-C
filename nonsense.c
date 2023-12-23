2bbf7f8cd050863df3d0e7ccec15ecd74056d34b
No need <No need>
2023-12-23 17:47:31 +0545

    Added Random-nonsense

---
+++ nonsense.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+
+int main() {
+	char USERNAME[] = "krishna";
+	char PASSWORD[] = "pass";
+	
+	char username[8], password[8];
+
+	int legit;
+
+	printf("Login Form\n Enter Your UserName\n");
+	scanf("%s", username);
+
+	legit=strcmp(USERNAME,username);
+
+	if(legit==0) {
+		printf("Enter Password\n");
+		scanf("%s",password);
+		if (strcmp(PASSWORD,password)==0) {
+			printf("Login Successful\n");
+		}
+		else{
+			printf("Incorrect Password\n");
+		}
+	}
+	else{
+		printf("Incorrect Username\n");
+	}
+
+	return 0;
+}
